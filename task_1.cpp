046f6f9636d198cdc05a0ed1556a74b5c6fc2c21
magnus <StoneSwine@users.noreply.github.com>
2020-09-17 13:10:29 +0200

    Cleaning up output

--- task_1.cpp
+++ task_1.cpp
@@ -11,12 +11,19 @@
 
 using namespace std;
 
-/* Dynamic programming approach to approximate search
+/**
+ * Dynamic programming approach to approximate search
  * O(nm) complexity with O(mn) space
+ *
+ * @param P
+ * @param T
+ * @param k
+ * @return
  */
-auto DPSearchWithFullMatrix(const string &p, const string &T, const int k) {
-    auto m = p.length() + 1, n = T.length() + 1;
-    u_long minDist = UINTMAX_MAX;
+bool DPSearchWithFullMatrix(const string &P, const string &T, const uint k) {
+    auto m = P.length() + 1, n = T.length() + 1;
+    bool match = false;
+
     // Initialise the matrix
     int **M = new int *[m];
     for (int i = 0; i < m; ++i) {
@@ -24,20 +31,22 @@ auto DPSearchWithFullMatrix(const string &p, const string &T, const int k) {
         M[i][0] = i;
     }
 
-    printf("[DPSWFM]: Pattern --> %s\n", p.c_str());
+    printf("[DPSWFM]: Pattern: %s\n", P.c_str());
 
     // Operations on matrix.
     for (int j = 1; j < n; ++j) {
         for (int i = 1; i < m; ++i) {
-            if (p[i - 1] == T[j - 1]) { // match
+            if (P[i - 1] == T[j - 1]) { // match
                 M[i][j] = M[i - 1][j - 1];
             } else {
                 M[i][j] = 1 + min({M[i - 1][j - 1], M[i - 1][j], M[i][j - 1]});
             }
         }
-        if (M[m - 1][j] <= k && M[m - 1][j] < minDist) {
-            minDist = M[m - 1][j];
-       }
+        if (M[m - 1][j] <= k) {
+            printf("\t[DPSWOC]: Match found for %s and %s, at pos. %d with the cost %d\n", P.c_str(), T.c_str(),
+                   j - 1, k);
+            match = true;
+        }
     }
 
 #ifdef DEBUG
@@ -49,7 +58,7 @@ auto DPSearchWithFullMatrix(const string &p, const string &T, const int k) {
     cout << endl;
     for (int i = 0; i < m; ++i) {
         if (i != 0) {
-            cout << "\t" << p[i - 1];
+            cout << "\t" << P[i - 1];
         } else {
             cout << "\t ";
         }
@@ -64,23 +73,28 @@ auto DPSearchWithFullMatrix(const string &p, const string &T, const int k) {
 #endif
 
     //                        the size of the data + the size of the inner pointers + the size of the outer pointer
-    printf("[DPSWFM]: Size of data-structure: %lu B\n", m * n * sizeof(int) + m * sizeof M[0] + sizeof M);
+    printf("\t[DPSWFM]: Size of data-structure: %lu B\n", m * n * sizeof(int) + m * sizeof M[0] + sizeof M);
 
     // Free memory
     for (int i = 0; i < m; ++i)
         delete[] M[i];
     delete[] M;
 
-    return minDist;
+    return match;
 }
 
-
-/* Dynamic programming approach to approximate search
+/**
+ * Dynamic programming approach to approximate search
  * O(nm) complexity with O(m) space
+ * @param P
+ * @param T
+ * @param k
+ * @return
  */
-auto DPSearchWithOnlyColumns(const string &p, const string &T, const int k) {
-    auto m = p.size() + 1, n = T.size() + 1;
-    u_long minDist = UINTMAX_MAX;
+bool DPSearchWithOnlyColumns(const string &P, const string &T, const uint k) {
+    auto m = P.size() + 1, n = T.size() + 1;
+    bool match = false;
+
     int *C = new int[m];
     int *Cpr = new int[m];
     // initialise the vectors
@@ -88,52 +102,55 @@ auto DPSearchWithOnlyColumns(const string &p, const string &T, const int k) {
         C[i] = Cpr[i] = i;
     }
 
-    printf("[DPSWOC]: Pattern --> %s\n", p.c_str());
+    printf("[DPSWOC]: Pattern: %s\n", P.c_str());
 
     // operations on columns
     for (int j = 1; j < n; ++j) {
         for (int i = 1; i < m; ++i) {
-            if (p[i - 1] == T[j - 1]) { // match
+            if (P[i - 1] == T[j - 1]) { // match
                 Cpr[i] = C[i - 1];
             } else {    //       substitution, deletion, insertion
                 Cpr[i] = (1 + min({C[i - 1], Cpr[i - 1], C[i]}));
             }
         }
 
-        if (Cpr[m - 1] <= k && Cpr[m - 1] < minDist) {
-            minDist = Cpr[m - 1];
+        if (Cpr[m - 1] <= k) {
+            printf("\t[DPSWOC]: Match found for %s and %s, at pos. %d with the cost %d\n", P.c_str(), T.c_str(),
+                   j - 1, k);
+            match = true;
         }
         swap(C, Cpr);
 
     }
     //                                                 the size of the data   + the size of the outer pointer
-    printf("[DPSWOC]: Size of data-structure: %lu B\n", m * sizeof(int) + sizeof C);
+    printf("\t[DPSWOC]: Size of data-structure: %lu B\n", m * sizeof(int) + sizeof C);
 
     delete[] C;
     delete[] Cpr;
 
-    return minDist;
+    return match;
 }
+
 
 int main() {
     string patterns[] = {"tuned", "ThisIsTooDifferent"}; // Patterns (IDS DB)
     string T = "student"; // Search string
+    uint k = 2; // amount of differences
+
     printf("[MAIN] Starting search with full matrix in memory\n");
     printf("[MAIN] Search string: %s\n", T.c_str());
-    for (const auto &p : patterns) {
-        auto retDist = DPSearchWithFullMatrix(p, T, 2);
-        if (retDist != UINTMAX_MAX) {
-            printf("[MAIN]: ed(%s,%s)=%lu\n", p.c_str(), T.c_str(), retDist);
+    for (const auto &P : patterns) {
+        if (!DPSearchWithFullMatrix(P, T, k)) {
+            printf("[MAIN]: Match not found for %s and %s, with the cost %d\n", P.c_str(), T.c_str(), k);
         }
     }
     printf("- - - - - - - - - - - - - - - - \n");
 
     printf("[MAIN] Starting search with only columns in memory\n");
     printf("[MAIN] Search string: %s\n", T.c_str());
-    for (const auto &p : patterns) {
-        auto retDist = DPSearchWithOnlyColumns(p, T, 2);
-        if (retDist != UINTMAX_MAX) {
-            printf("[MAIN]: ed(%s,%s)=%lu\n", p.c_str(), T.c_str(), retDist);
+    for (const auto &P : patterns) {
+        if (!DPSearchWithOnlyColumns(P, T, k)) {
+            printf("[MAIN]: Match not found for %s and %s, with the cost %d\n", P.c_str(), T.c_str(), k);
         }
     }
     return 0;
